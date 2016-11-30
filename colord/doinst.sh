2f63dff6bf6143846ac2c24cd12d0aaa2dbc4fae
Willy Sudiarto Raharjo <willysr@slackware-id.org>
2016-11-30 18:39:35 +0700

    colord: Add user creation in the doinst.sh as well.
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackware-id.org>

--- colord/doinst.sh
+++ colord/doinst.sh
@@ -18,3 +18,8 @@ if [ -e usr/share/glib-2.0/schemas ]; then
     /usr/bin/glib-compile-schemas usr/share/glib-2.0/schemas >/dev/null 2>&1
   fi
 fi
+
+if ! grep -q ^colord: /etc/group ; then
+  groupadd -g 303 colord
+  useradd -d /var/lib/colord -u 303 -g colord -s /bin/false colord
+fi
